d2c18254fa575e37a962cf5f92cb66ce45cad290
Tura92 <daniel.tura@live.de>
2022-04-14 14:08:43 +0200

    added google form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 function App() {
-  return <div></div>;
+  return (
+    <div align="center">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLScY1hJudEcgjMO3E9Dc7Rga62ToKEEaMmkpXLilkRmWtsueqA/viewform?embedded=true"
+        width="640"
+        height="1386"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+        title="Visus Form"
+      >
+        Wird geladen…
+      </iframe>
+    </div>
+  );
 }
 
 export default App;
