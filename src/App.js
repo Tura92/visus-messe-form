b4b6da3d9790c9422e9dea732ffb4d78fd05de7f
Tura92 <daniel.tura@live.de>
2022-04-15 00:47:21 +0200

    remove double scrollbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import "./App.scss";
+
 function App() {
   return (
-    <div align="center">
+    <div className="form-container" align="center">
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLScY1hJudEcgjMO3E9Dc7Rga62ToKEEaMmkpXLilkRmWtsueqA/viewform?embedded=true"
         width="640"
-        height="1386"
+        height="1450"
         frameborder="0"
         marginheight="0"
         marginwidth="0"
+        scrolling="no"
         title="Visus Form"
       >
         Wird geladen…
