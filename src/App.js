f807a16553c578731094831605acf19fda872d31
Tura92 <daniel.tura@live.de>
2022-04-19 02:43:05 +0200

    restyle page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import "./App.scss";
+import logo from "./resources/visus_logo.jpg";
 
 function App() {
   return (
-    <div className="form-container" align="center">
-      <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLSe5pXbgUFO_XoXY84kTFZopUfMziiUB7yE4YUA0t0P1Rtwz6Q/viewform?embedded=true"
-        width="640"
-        height="1450"
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
-        scrolling="no"
-        title="Visus Form"
-      >
-        Wird geladen…
-      </iframe>
+    <div className="row" align="center">
+      <div>
+        <img src={logo} alt="Girl in a jacket" width="576" />
+      </div>
+      <div>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSe5pXbgUFO_XoXY84kTFZopUfMziiUB7yE4YUA0t0P1Rtwz6Q/viewform?embedded=true"
+          width="640"
+          height="1450"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+          scrolling="no"
+          title="Visus Form"
+        >
+          Wird geladen…
+        </iframe>
+      </div>
     </div>
   );
 }
