0e3513c9d397579c0345abfce559937260ee09e8
Tura92 <daniel.tura@live.de>
2022-04-19 03:41:51 +0200

    omptimize iframe heigth

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSe5pXbgUFO_XoXY84kTFZopUfMziiUB7yE4YUA0t0P1Rtwz6Q/viewform?embedded=true"
           width="640"
-          height="1450"
+          // height="1450"
           frameBorder="0"
           marginHeight="0"
           marginWidth="0"
-          scrolling="no"
+          //scrolling="no"
           title="Visus Form"
         >
           Wird geladen…
